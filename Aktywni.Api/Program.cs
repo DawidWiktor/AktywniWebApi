89e9bf88b9476a90b15d6e0b6af16e4d2dbe9b51
DawidWiktor <dawidnfs@gmail.com>
2018-05-21 14:11:25 +0200

    baza danych aktualna

--- Aktywni.Api/Program.cs
+++ Aktywni.Api/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -15,6 +16,18 @@ namespace Aktywni.Api
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+   /*         string currentProjectPath = Environment.CurrentDirectory;
+            var host = new WebHostBuilder()
+    .UseKestrel(options =>
+    {
+        options.Listen(IPAddress.Parse("127.0.0.1"), 443, listenOptions => 
+        {
+            listenOptions.UseHttps(currentProjectPath + "\\certificate.pfx", "poklikasz1");
+        });
+    })
+    .UseStartup<Startup>()
+    .Build();
+    host.Run();*/
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
